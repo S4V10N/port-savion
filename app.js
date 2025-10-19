acff6fcbb6632ce86eceabbafa49a5c21281dc1f
savion <didiapraisechimeka@gmail.com>
2025-10-19 21:48:30 +0100

    added scroll animation

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ console.log("js is working");
 const seeMore = document.querySelector("#expand");
 const seeLess = document.querySelector("#shrink");
 const accordion = document.querySelector(".projects__expand");
+const menu = document.querySelector(".hamburger")
+const menuHand = document.querySelectorAll(".hamburger div");
 
 seeMore.addEventListener("click", () => {
     accordion.classList.add("visible");
@@ -9,5 +11,23 @@ seeMore.addEventListener("click", () => {
 })
 seeLess.addEventListener("click" ,() => {
     accordion.classList.remove("visible");
-        seeMore.classList.remove("hidden");
+    seeMore.classList.remove("hidden");
+    window.scrollTo({
+        top: 3000,
+        left: 0,
+        behavior: "smooth"
+    });
+})
+function xBtn () {
+    menuHand[0].classList.add("x-one")
+    menuHand[1].classList.add("x-none")
+    menuHand[2].classList.add("x-two")
+}
+function hamburgerBtn () {
+    menuHand[0].classList.remove("x-one")
+    menuHand[1].classList.remove("x-none")
+    menuHand[2].classList.remove("x-two")
+}
+window.addEventListener('load', () => {
+    window.scrollTo(0, 0);
 })
