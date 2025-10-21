8bd9b7c65eb5984ab76cca4a090f0853ac3a4077
savion <didiapraisechimeka@gmail.com>
2025-10-21 12:30:36 +0100

    added dropdown

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-console.log("js is working");
+window.addEventListener('load', () => {
+    window.scrollTo(0, 0);
+})
 const seeMore = document.querySelector("#expand");
 const seeLess = document.querySelector("#shrink");
 const accordion = document.querySelector(".projects__expand");
 const menu = document.querySelector(".hamburger")
 const menuHand = document.querySelectorAll(".hamburger div");
+const hamburgerMenu = document.querySelector(".hamburger__menu");
+const nav = document.querySelector("nav");
 
 seeMore.addEventListener("click", () => {
     accordion.classList.add("visible");
@@ -12,22 +16,32 @@ seeMore.addEventListener("click", () => {
 seeLess.addEventListener("click" ,() => {
     accordion.classList.remove("visible");
     seeMore.classList.remove("hidden");
-    window.scrollTo({
-        top: 3000,
-        left: 0,
-        behavior: "smooth"
-    });
 })
-function xBtn () {
+function xBtn() {
     menuHand[0].classList.add("x-one")
     menuHand[1].classList.add("x-none")
     menuHand[2].classList.add("x-two")
 }
-function hamburgerBtn () {
+function hamburger() {
     menuHand[0].classList.remove("x-one")
     menuHand[1].classList.remove("x-none")
     menuHand[2].classList.remove("x-two")
 }
-window.addEventListener('load', () => {
-    window.scrollTo(0, 0);
-})
+function dropDown() {
+    hamburgerMenu.style.display = "flex";
+    xBtn()
+}
+function closeBtn() {
+    hamburgerMenu.style.display = "none";
+    hamburger()
+}
+function openTab() {
+    menu.addEventListener("click", () => {
+    dropDown();
+    menu.addEventListener("click", () => {
+        closeBtn();
+        openTab();
+    })
+    });
+}
+openTab();
