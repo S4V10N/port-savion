940bb121dee977920c08b53ab8b7d998ae970e4b
savion <didiapraisechimeka@gmail.com>
2025-10-21 22:23:24 +0100

    added menu

--- app.js
+++ app.js
@@ -1,47 +1,54 @@
-window.addEventListener('load', () => {
-    window.scrollTo(0, 0);
-})
+window.addEventListener("load", () => {
+  window.scrollTo(0, 0);
+});
 const seeMore = document.querySelector("#expand");
 const seeLess = document.querySelector("#shrink");
 const accordion = document.querySelector(".projects__expand");
-const menu = document.querySelector(".hamburger")
+const menu = document.querySelector(".hamburger");
 const menuHand = document.querySelectorAll(".hamburger div");
 const hamburgerMenu = document.querySelector(".hamburger__menu");
+const hamburgerLinks = document.querySelectorAll(".hamburger__menu div a");
 const nav = document.querySelector("nav");
 
 seeMore.addEventListener("click", () => {
-    accordion.classList.add("visible");
-    seeMore.classList.add("hidden");
-})
-seeLess.addEventListener("click" ,() => {
-    accordion.classList.remove("visible");
-    seeMore.classList.remove("hidden");
-})
+  accordion.classList.add("visible");
+  seeMore.classList.add("hidden");
+});
+seeLess.addEventListener("click", () => {
+  accordion.classList.remove("visible");
+  seeMore.classList.remove("hidden");
+});
 function xBtn() {
-    menuHand[0].classList.add("x-one")
-    menuHand[1].classList.add("x-none")
-    menuHand[2].classList.add("x-two")
+  menuHand[0].classList.add("x-one");
+  menuHand[1].classList.add("x-none");
+  menuHand[2].classList.add("x-two");
 }
 function hamburger() {
-    menuHand[0].classList.remove("x-one")
-    menuHand[1].classList.remove("x-none")
-    menuHand[2].classList.remove("x-two")
+  menuHand[0].classList.remove("x-one");
+  menuHand[1].classList.remove("x-none");
+  menuHand[2].classList.remove("x-two");
 }
 function dropDown() {
-    hamburgerMenu.style.display = "flex";
-    xBtn()
+  hamburgerMenu.style.display = "flex";
+  xBtn();
 }
 function closeBtn() {
-    hamburgerMenu.style.display = "none";
-    hamburger()
+  hamburgerMenu.style.display = "none";
+  hamburger();
 }
 function openTab() {
-    menu.addEventListener("click", () => {
+  menu.addEventListener("click", () => {
     dropDown();
     menu.addEventListener("click", () => {
-        closeBtn();
-        openTab();
-    })
+      closeBtn();
+      openTab();
     });
+  });
 }
 openTab();
+
+for (let i = 0; i < hamburgerLinks.length; i++) {
+  hamburgerLinks[i].addEventListener("click", () => {
+    closeBtn();
+  });
+}
